be712a5d3187e0dd9350f8fda8ed57e6c429217a
amrron <imrona463@gmail.com>
2024-09-16 13:12:09 +0700

    feat: admin sub domain

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,20 @@ use Illuminate\Support\Facades\Route;
 //     return view(view: 'pages.home');
 // });
 
-
-Route::controller(GeneralController::class)->group(function() {
-    Route::get('/', 'home')->name('user.home');
-    Route::get('/about-us', 'aboutUs')->name('user.about-us');
-    Route::get('/team', 'team')->name('user.team');
-    Route::get('/faq', 'faq')->name('user.faq');
-    Route::get('/contact-us', 'contactUs')->name('user.contact-us');
-    Route::get('/blog', 'blog')->name('user.blog');
-    Route::get('/blog/detail', 'blogDetail')->name('user.blog.detail');
+Route::domain(env('APP_DOMAIN', "maggoplastgel.test"))->group(function () {
+    Route::controller(GeneralController::class)->group(function() {
+        Route::get('/', 'home')->name('user.home');
+        Route::get('/about-us', 'aboutUs')->name('user.about-us');
+        Route::get('/team', 'team')->name('user.team');
+        Route::get('/faq', 'faq')->name('user.faq');
+        Route::get('/contact-us', 'contactUs')->name('user.contact-us');
+        Route::get('/blog', 'blog')->name('user.blog');
+        Route::get('/blog/detail', 'blogDetail')->name('user.blog.detail');
+    });
 });
 
-
+Route::domain('admin.' . env('APP_DOMAIN', "maggoplastgel.test"))->group(function () {
+    Route::get('/', function () {
+        return 'Admin sub domain';
+    });
+});
